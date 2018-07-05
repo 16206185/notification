1ef76eb7afa4d519419c93b5b920f527c787d5ba
Wan <tom3647@gmail.com>
2018-07-05 16:31:34 +0800

    Notv2

--- NotificationDemo/app/src/main/java/com/example/bf6193/notificationdemo/MyFirebaseMessagingService.java
+++ NotificationDemo/app/src/main/java/com/example/bf6193/notificationdemo/MyFirebaseMessagingService.java
@@ -1,7 +1,9 @@
 package com.example.bf6193.notificationdemo;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
@@ -14,8 +16,14 @@ import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 
+
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     RequestQueue queue;
+    Notification myNotication;
+    NotificationManager mNotificationManager;
+    NotificationCompat.Builder mBuilder;
+
+
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         Log.d("FCM", "onMessageReceived:"+remoteMessage.getNotification().getBody());
@@ -23,7 +31,29 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     }
 
     private void sendNotification(String messageBody) {
-        Intent intent = new Intent(this, MainActivity.class);
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark)
+                        .setContentTitle(messageBody)
+                        .setContentText("Hello World!");
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(0, mBuilder.build());
+
+
+        /*Intent intent = new Intent(this, MainActivity.class);
+
+
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
                 PendingIntent.FLAG_ONE_SHOT);
@@ -40,6 +70,6 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager.notify(0 , notificationBuilder.build());
+        notificationManager.notify(0 , notificationBuilder.build());*/
     }
 }
