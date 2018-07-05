92c2eba65f1f0b51b406eacc301bacc5641d0ac3
Wan <tom3647@gmail.com>
2018-07-05 15:16:18 +0800

    Token

--- NotificationDemo/app/src/main/java/com/example/bf6193/notificationdemo/MainActivity.java
+++ NotificationDemo/app/src/main/java/com/example/bf6193/notificationdemo/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.bf6193.notificationdemo;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +17,8 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import android.provider.Settings.Secure;
+
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -32,11 +37,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         queue = Volley.newRequestQueue(this);
         Button button3 = (Button)findViewById(R.id.button3);
+
         button3.setOnClickListener(new Button.OnClickListener(){
             @Override
 
             public void onClick(View v) {
-                connectionNot();
+                String androidId = Settings.Secure.getString(getContentResolver(),
+                        Settings.Secure.ANDROID_ID);
+                Log.d("UnqieId",androidId);
+
+                //connectionNot();
             }
         });
     }
@@ -50,42 +60,6 @@ public class MainActivity extends AppCompatActivity {
             Log.d("FCM", "msg:"+msg);
     }
 
-    public void connectionNot() {
-        String url = "http://192.9.204.143/get_message.php";
-        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject jsonObj = new JSONObject(response);
-                            Log.d("myTag", jsonObj.toString());
 
-                            if (jsonObj.getString("success").equals("1")) {
-                                Log.d("Yse","1111111111");
-                            } else {
-                                Log.d("No","22222222222");
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // error
-                        Log.d("Error.Response", "ERROR");
-                    }
-                }
-        ) {
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("PhoneNumber", "");
-                return params;
-            }
-        };
-        queue.add(postRequest);
-    }
 
 }
--- NotificationDemo/app/src/main/java/com/example/bf6193/notificationdemo/MyInstanceIDService.java
+++ NotificationDemo/app/src/main/java/com/example/bf6193/notificationdemo/MyInstanceIDService.java
@@ -1,22 +1,81 @@
 package com.example.bf6193.notificationdemo;
 
+import android.provider.Settings;
 import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static android.content.ContentValues.TAG;
 
 public class MyInstanceIDService extends FirebaseInstanceIdService {
+    RequestQueue queue;
+    String NotificationToken = "";
+    String androidId = "";
     @Override
     public void onTokenRefresh() {
         // Get updated InstanceID token.
+        queue = Volley.newRequestQueue(this);
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         Log.d(TAG, "Refreshed token: " + refreshedToken);
-
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
         // Instance ID token to your app server.
-        //sendRegistrationToServer(refreshedToken);
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    public void sendRegistrationToServer(String refreshedToken) {
+        NotificationToken = refreshedToken;
+        androidId = Settings.Secure.getString(getContentResolver(),
+                Settings.Secure.ANDROID_ID);
+
+        String url = "http://192.9.204.143/returnNotifcToken.php";
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObj = new JSONObject(response);
+                            Log.d("myTag", jsonObj.toString());
+
+                            if (jsonObj.getString("success").equals("1")) {
+                                Log.d("Yse","1111111111");
+                            } else {
+                                Log.d("No","22222222222");
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d("Error.Response", "ERROR");
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("DeviceId", androidId);
+                params.put("NotificationToken", NotificationToken);
+                return params;
+            }
+        };
+        queue.add(postRequest);
     }
 }
