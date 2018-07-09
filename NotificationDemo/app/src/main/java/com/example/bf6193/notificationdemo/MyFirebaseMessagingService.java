6815302900d1b64b47eba98d04238ae2fdfbc261
Wan <tom3647@gmail.com>
2018-07-09 10:35:41 +0800

    Final2

--- NotificationDemo/app/src/main/java/com/example/bf6193/notificationdemo/MyFirebaseMessagingService.java
+++ NotificationDemo/app/src/main/java/com/example/bf6193/notificationdemo/MyFirebaseMessagingService.java
@@ -1,6 +1,7 @@
 package com.example.bf6193.notificationdemo;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
@@ -8,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -34,49 +36,49 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         String[] messageBodyList = message.split(",");
         Nid = messageBodyList[0];
         messageBody = messageBodyList[1];
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(this)
-                        .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark)
-                        .setContentTitle(messageBody)
-                        .setContentText("Hello World!");
+
+
         Intent resultIntent = new Intent(this, MainActivity.class);
         resultIntent.putExtra("Nid", Nid);
-
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
         stackBuilder.addParentStack(MainActivity.class);
         stackBuilder.addNextIntent(resultIntent);
 
-        PendingIntent resultPendingIntent =
+        PendingIntent pendingIntent =
                 stackBuilder.getPendingIntent(
                         0,
                         PendingIntent.FLAG_UPDATE_CURRENT
                 );
-        mBuilder.setContentIntent(resultPendingIntent);
-        mBuilder.setAutoCancel(true);
-        NotificationManager mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        mNotificationManager.notify(0, mBuilder.build());
 
+        NotificationManager manager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
 
-        /*Intent intent = new Intent(this, MainActivity.class);
+        if(Build.VERSION.SDK_INT >= 26)
+        {
+            String id = "channel_1";
+            String description = "143";
+            int importance = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel channel = new NotificationChannel(id, description, importance);
+            manager.createNotificationChannel(channel);
+            Notification notification = new Notification.Builder(this, id)
+                    .setCategory(Notification.CATEGORY_MESSAGE)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle(messageBody)
+                    .setContentText("Hello world")
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true)
+                    .build();
+            manager.notify(1, notification);
+        }
+        else
+        {
+            Notification notification = new NotificationCompat.Builder(this)
+                    .setContentTitle(messageBody)
+                    .setContentText("Hello world")
+                    .setContentIntent(pendingIntent)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .build();
+            manager.notify(1,notification);
+        }
 
-
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
-                PendingIntent.FLAG_ONE_SHOT);
-
-        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark)
-                .setContentTitle("FCM Message")
-                .setContentText(messageBody)
-                .setAutoCancel(true)
-                .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent);
-
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        notificationManager.notify(0 , notificationBuilder.build());*/
     }
 }
